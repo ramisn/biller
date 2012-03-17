66cb668a0eaf045e6470fb31722d046a1835c013
Nand Kishore <fstech@fstech057.(none)>
2012-03-17 12:55:41 +0530

    test2_migration

--- db/migrate/20120119103511_add_fields_to_products.rb
+++ db/migrate/20120119103511_add_fields_to_products.rb
@@ -1,6 +1,6 @@
 class AddFieldsToProducts < ActiveRecord::Migration
   def change
     add_column :products, :key, :string, :null => false, :default => ""
-    add_column :products, :description , :text, :null => true, :limit => 255
+    add_column :products, :description , :string 
   end
 end
